d04ecf64b1bdd87758cbdfc58002e6845a0b3d37
wireless911 <925594949@qq.com>
2020-08-19 03:24:35 +0800

    [更新] 合并

--- actions/actions.py
+++ actions/actions.py
@@ -9,9 +9,6 @@ from rasa_sdk.executor import CollectingDispatcher
 from sanic.log import logger as _logger
 
 
-
-
-
 class ExpressStoreHouse(Action):
     """快递仓库查询"""
 
@@ -25,5 +22,7 @@ class ExpressStoreHouse(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         msg = """亲爱的，店铺正装商品订单使用「顺丰陆运」从苏州包邮寄出，顺丰无法到达区域，将会用其他承运快递公司发出。（无法指定快递发出）"""
+        dispatcher.utter_message(msg)
+        msg = "顺丰速运，常伴吾身"
         dispatcher.utter_message(msg)
         return ([AllSlotsReset()])
